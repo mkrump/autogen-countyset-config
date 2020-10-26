cca06d9244cc32cb8710956667e6f3c3dd14e4be
Matt Krump <1036969+mkrump@users.noreply.github.com>
2020-10-26 13:51:27 -0600

    Extract functions

--- main.py
+++ main.py
@@ -24,15 +24,7 @@ def read_csv(filename):
         return county_sets
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Autogenerate config file based on county data')
-    parser.add_argument('--input-file', dest='input_file', required=True, type=str,
-                        help='csv file with county name and fips data')
-    parser.add_argument('--output-dir', dest='output_dir', required=True, type=str, help='output directory location')
-
-    args = parser.parse_args()
-    output_dir = args.output_dir
-    county_sets = read_csv(args.input_file)
+def generate_state_configs(county_sets):
     expected_headers = {'State', 'COUNTYFP', 'StateCode'}
     file_headers = county_sets[0].keys()
     if len(expected_headers.intersection(file_headers)) != 3:
@@ -45,8 +37,26 @@ if __name__ == '__main__':
     county_sets = sorted(county_sets, key=lambda x: (x['State'], x['COUNTYFP']))
     states = itertools.groupby(county_sets, key=lambda x: x['State'])
     for state_name, state in states:
-        rendered_template = CONFIG_TEMPLATE.render(county_sets=list(state))
-        filename = f'{output_dir}/{state_name.lower()}-gfts-conf.py'
-        with open(filename, "w") as f:
-            logging.info(f'generating: {filename}')
-            f.write(rendered_template)
+        generate_config(state, f"{state_name.lower()}-gfts-conf.py")
+
+
+def generate_config(county_sets, filename):
+    rendered_template = CONFIG_TEMPLATE.render(county_sets=list(county_sets))
+    filename = f'{output_dir}/{filename}'
+    with open(filename, "w") as f:
+        logging.info(f'generating: {filename}')
+        f.write(rendered_template)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Autogenerate config file based on county data')
+    parser.add_argument('--input-file', dest='input_file', required=True, type=str,
+                        help='csv file with county name and fips data')
+    parser.add_argument('--output-dir', dest='output_dir', required=True, type=str, help='output directory location')
+
+    args = parser.parse_args()
+    output_dir = args.output_dir
+    input_file = args.input_file
+
+    all_county_sets = read_csv(input_file)
+    generate_state_configs(all_county_sets)
